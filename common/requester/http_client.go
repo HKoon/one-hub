a61aa6df22001fb0c6be6b1053bed804d57c39ed
Hillay Koon <iace@foxmail.com>
2025-08-06 14:57:12 +0800

    fix: simplify HTTP client configuration to resolve connection reset issues

--- common/requester/http_client.go
+++ common/requester/http_client.go
@@ -12,34 +12,14 @@ func InitHttpClient() {
 	trans := &http.Transport{
 		DialContext: utils.Socks5ProxyFunc,
 		Proxy:       utils.ProxyFunc,
-
-		// 连接池配置
-		MaxIdleConns:        100,              // 最大空闲连接数
-		MaxIdleConnsPerHost: 20,               // 每个主机最大空闲连接数
-		MaxConnsPerHost:     50,               // 每个主机最大连接数
-		IdleConnTimeout:     90 * time.Second, // 空闲连接超时时间
-
-		// 超时配置
-		TLSHandshakeTimeout:   10 * time.Second,  // TLS握手超时
-		ResponseHeaderTimeout: 180 * time.Second, // 响应头超时 - 关键配置
-		ExpectContinueTimeout: 1 * time.Second,   // Expect: 100-continue超时
-
-		// 保持连接活跃
-		DisableKeepAlives: false,
-
-		// 启用HTTP/2
-		ForceAttemptHTTP2: true,
 	}
 
 	HTTPClient = &http.Client{
 		Transport: trans,
 	}
 
-	relayTimeout := utils.GetOrDefault("relay_timeout", 0)
-	if relayTimeout > 0 {
+	relayTimeout := utils.GetOrDefault("relay_timeout", 600)
+	if relayTimeout != 0 {
 		HTTPClient.Timeout = time.Duration(relayTimeout) * time.Second
-	} else {
-		// 如果没有设置relay_timeout，使用默认的300秒超时
-		HTTPClient.Timeout = 300 * time.Second
 	}
 }
